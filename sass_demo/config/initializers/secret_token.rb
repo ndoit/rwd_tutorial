bb9032f9f500e7d8c27105ea8e969e5281d63453
brandonrich <brich@Brandons-MacBook-Pro-2.local>
2014-02-21 20:37:27 -0500

    initial revision

---
+++ sass_demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SassDemo::Application.config.secret_key_base = '915949e149e671e4789b381558d0fad43e74be27a24831a043e515f7443fed7f5778b6f2aa56c7e5caf9d9b0991ce567fac7242e3a6e59ea464af94f922ee104'
---
+++ sass_demo/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SassDemo::Application.config.session_store :cookie_store, key: '_sass_demo_session'
